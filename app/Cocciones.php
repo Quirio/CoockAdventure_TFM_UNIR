e40b05ddfd5123fd57b85786f915044a4ad6227c
Alejandro Hernández Hernández <Quirio123@gmail.com>
2016-09-03 20:56:26 +0200

    Inserción tipo de Cocción / Cooking method insert

---
+++ app/Cocciones.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cocciones extends Model
+{
+    protected $table = 'tipo_coccion';
+}
---
+++ app/Http/CapaNegocio/Coccion.php
@@ -0,0 +1,18 @@
+<?php
+	use App\Cocciones;
+	class Coccion{
+		function insert($nombre){			
+			$Coincidencia = Cocciones::where('nombre','=', $nombre)->first();
+
+			if(empty($Coincidencia)){				
+				$coccion = new Cocciones;
+				$coccion->nombre = $nombre;
+				$coccion->total_registros = 1;
+				$coccion->save();
+			}else{
+				$Coincidencia->increment('total_registros');
+				$Coincidencia->save();
+			}
+		}
+	}
+?>
